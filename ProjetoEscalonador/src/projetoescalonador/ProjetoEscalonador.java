8b9c77a5e48a25c9da8bbc466dafe14a52a1ae9b
Ewerton Duarte <joaosouza@alunos.utfpr.edu.br>
2018-05-28 22:38:00 -0300

    Primeiro commit2

--- ProjetoEscalonador/src/projetoescalonador/ProjetoEscalonador.java
+++ ProjetoEscalonador/src/projetoescalonador/ProjetoEscalonador.java
@@ -15,7 +15,7 @@ public class ProjetoEscalonador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("sim");
     }
     
 }
